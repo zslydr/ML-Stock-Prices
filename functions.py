61f115c7d6226b3d2739728eaa1122bccfeded41
zslydr <30627785+zslydr@users.noreply.github.com>
2018-09-25 14:12:47 +0200

    code plut complet

--- functions.py
+++ functions.py
@@ -12,17 +12,18 @@ cwd = os.getcwd()
 import pandas as pd
 import numpy as np
 
-def transform_data(time_serie, lag_input, target_day):
+def transform_data(time_serie, lag_input, predict_from, predict_to):
     
     X = []
     Y = []
     
-    for i in range(lag_input, time_serie.shape[0]):
+    for i in range(lag_input, time_serie.shape[0]-(predict_to - predict_from)+1):
         X.append(time_serie[i-lag_input:i])
-        Y.append(time_serie[i])
+        Y.append(time_serie[i + predict_from : i + predict_to])
         
     X = np.array(X)
     Y = np.array(Y)
     
     X = X.reshape((X.shape[0], X.shape[1], 1))
+    Y = Y.reshape((Y.shape[0], predict_to - predict_from))
     return(X, Y)
